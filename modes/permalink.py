c78121c12ed2271d692750bf0bcabc9f4d707ea9
Adam Hennessy <ahenneuk@live.com>
2026-02-03 00:31:21 +0000

    move page functions out of main script

---
+++ modes/permalink.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+from functions import link_from_permalink, save_permalink
+
+def page_permalink(playlist_id):
+
+    playlist_url = link_from_permalink(playlist_id)
+    print(playlist_url)
+
+
+    if playlist_url:
+        st.button('YouTube')
+
+    else:
+        st.error(":red[**ERROR**:] Permalink not found or has expired")
+        
+    return
