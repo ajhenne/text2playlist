b1cf137c33bcf29d8b39fa6b3bbceeba79ad81c0
Adam Hennessy <ahenneuk@live.com>
2026-02-17 18:01:52 +0000

    convert name to text2playlist

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
-from modes.chat2playlist import page_chat2playlist
+from modes.text2playlist import page_text2playlist
 from modes.permalink import page_permalink
 from modes.privacy import page_privacy
 
 from functions.css import (custom_css,
                            footer_css,
                            dialog_css)
+
+st.set_page_config(page_title="text2playlist")
+
 
 if "processed_data" not in st.session_state:
     st.session_state.processed_data = None
@@ -15,6 +18,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 st.markdown(dialog_css, unsafe_allow_html=True)
 st.markdown(footer_css, unsafe_allow_html=True)
 
+
 if "p" in st.query_params:
     page_permalink(st.query_params['p'])
 
@@ -22,7 +26,7 @@ elif st.query_params.get('v') == 'privacy':
     page_privacy()
     
 else:
-    page_chat2playlist()
+    page_text2playlist()
 
 
 
